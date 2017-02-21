d3fb085d9952d19968b7648998ac7bcd22e9a815
song <song@song-Vostro-1400.(none)>
2017-02-21 22:53:45 +0800

    add name_cases.py

---
+++ name_cases.py
@@ -0,0 +1,23 @@
+name = 'eric'
+
+print("Hello", name.title()+','+'would you like python.')
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print('Albert Einstein once said, " A person who never make a mistake never tried anything new"')
+	
+famous_person = "Alvert Einstein"
+said = "A person who never made a mistake never tryed anything new."
+message = famous_person + ',' + said
+print(message)
+
+name_person = "\tAlvert Einstein\n"
+print(name_person)
+print(name_person.lstrip())
+print(name_person.rstrip())
+print(name_person.strip())
+print("end")
+
+
