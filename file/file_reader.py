9aecf66cc20decee8d2df50fe49ec7441839671b
songjy <sjyfok@163.com>
2017-07-21 13:16:59 +0800

    学习异常处理

--- file/file_reader.py
+++ file/file_reader.py
@@ -5,8 +5,18 @@ with open('pi_digits.txt') as file_object:
 
 print()
 
-filename = 'pi.digits.txt'
+filename = 'pi_digits.txt'
 
-with open(filename) as file_object:
-	for line in file_object:
-		print(line)
+with open(filename) as file_object1:
+	for line in file_object1:
+		print(line.rstrip())
+
+
+print()
+
+with open(filename) as file_object2:
+	lines = file_object2.readlines()
+
+for line in lines:
+	print(line.rstrip())
+	
