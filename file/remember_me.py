7528dbe2ac9b5b7cd3261d2f7ad68e3083820ea4
songjy <sjyfok@163.com>
2017-07-25 08:31:35 +0800

    文件和异常部分完成

--- file/remember_me.py
+++ file/remember_me.py
@@ -1,5 +1,35 @@
 import json
 
+def greet_user():
+#	filename = 'username.json'
+	username = get_stored_username()
+	if username:
+		print("Wecome back, "+username+"!")
+	else :
+		username = get_new_username()
+		print("We'll remember you when you come back, " + username + '!')
+
+def get_new_username():
+	username = input("What is your name?")
+	filename = 'username.json'
+	with open(filename, 'w') as f_obj:
+		json.dump(username, f_obj)
+	return username
+	
+def get_stored_username():
+	filename = 'username.json'
+	try:
+		with open(filename) as f_obj:
+			username = json.load(f_obj)
+	except FileNotFoundError:
+		return None
+	else:
+		return username
+
+
+greet_user()
+
+'''
 #username = input("What is your name?")
 
 filename = 'username.json'
@@ -14,3 +44,4 @@ except FileNotFoundError:
 else:
 	print("Welcome back, " + username + "!")
 	
+'''
