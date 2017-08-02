01223f126b967ccf25323053a1f3dca5299e405f
songjy <sjyfok@163.com>
2017-08-02 15:58:27 +0800

    可以增加设计功能

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	pygame.init()
@@ -10,17 +10,17 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 	
 	bg_color = (230, 230, 230)
 	
 
 	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
+		gf.check_events(ship)
+		ship.update()
+		#screen.fill(ai_settings.bg_color)
+		#ship.blitme()
+		gf.update_screen(ai_settings, screen, ship)
 		pygame.display.flip()
 
 
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,7 +1,9 @@
 class Settings():
 
 	def __init__(self):
-		self.screen_width = 1200
-		self.screen_height = 800
+		self.screen_width = 1024
+		self.screen_height = 728
 		self.bg_color = (230, 230, 230)
+
+		self.ship_speed_factor = 1.5
 		
