a389288f495a9fa1210ec2faa600ae0c3e68dd1a
songjy <sjyfok@163.com>
2017-08-05 16:44:34 +0800

    可以射击移动

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,20 +14,20 @@ def run_game():
 	ship = Ship(ai_settings, screen)
 	bullets = Group()
 	bg_color = (230, 230, 230)
+
+	#alien = Alien(ai_settings, screen)
+	aliens = Group()
+
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 
 	while True:
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		bullets.update()
-
-		for bullet in bullets.copy():
-			if bullet.rect.bottom <= 0:
-				bullets.remove(bullet)
-		print(len(bullets))
+		gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+		gf.update_aliens(ai_settings, aliens)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 		
-		gf.update_screen(ai_settings, screen, ship, bullets)
-		pygame.display.flip()
 
 
 run_game()
