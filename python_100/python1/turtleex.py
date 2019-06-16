8faa8a595697ceeed551a031f84e8aadd06c9fa6
song <songjyf@126.com>
2019-06-16 11:08:15 +0800

    增加python_100目录

---
+++ python_100/python1/turtleex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#一定要放在首行
+
+'''
+如果提示No module name tkinter
+需要执行 sudo apt install python3-tk
+'''
+
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+
